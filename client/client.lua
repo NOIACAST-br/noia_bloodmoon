f37f2381b296cb91846c5ca27373f6d9a34014a7
NOIACAST <lucasmongconann24@gmail.com>
2024-11-02 20:15:52 -0300

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -14,8 +14,17 @@ AddEventHandler('setHalloweenWeather', function(weatherType)
     N_0x3373779baf7caf48("FOG", "fog_MP_Pred")
 end)
 
-RegisterNetEvent('setNormalWeather')
-AddEventHandler('setNormalWeather', function()
+RegisterNetEvent('setNormalWeatherWithTransition')
+AddEventHandler('setNormalWeatherWithTransition', function(transitionTime)
+    local transition_time_in_seconds = transitionTime or 30.0
+    local clearWeatherType = "CLEAR"
+    local next_weather_type_hash = GetHashKey(clearWeatherType)
+    
+    Citizen.InvokeNative(0x59174F1AFE095B5A, next_weather_type_hash, true, true, true, transition_time_in_seconds, false)
     ClearOverrideWeather()
     ClearWeatherTypePersist()
+end)
+
+AddEventHandler('playerSpawned', function()
+    TriggerServerEvent('requestClockHours')
 end)
